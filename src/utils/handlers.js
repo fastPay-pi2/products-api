03b3e8d2bf58d9f658e4de2f63d64cf383e3fa1d
João Vitor <joaovytor0@gmail.com>
2019-12-06 01:49:32 -0300

    Add beautiful products

--- src/utils/handlers.js
+++ src/utils/handlers.js
@@ -12,6 +12,34 @@ const handleResponse = (result, response) => {
   }
 }
 
+const handleBeautifulProduct = (result, response) => {
+  if (result.error) {
+    response.status(400).json({ error: result.error })
+  } else if (result.res.rowCount > 0) {
+    if (result.message) {
+      response.status(200).json({ msg: result.message })
+    } else {
+      var aux = new Object()
+      var beautifulProducts = []
+      const products = result.res.rows
+      for (product in products){
+        aux[products[product]['idsubcategory']] = 0
+      }
+      for (product in products){
+        if (aux[products[product]['idsubcategory']] < 4){
+          beautifulProducts.push(products[product]);
+          aux[products[product]['idsubcategory']] += 1;
+        }
+      }
+      console.log(beautifulProducts.length)
+      response.status(200).json(beautifulProducts)
+    }
+  } else {
+    response.status(404).json({ msg: 'Not Found' })
+  }
+}
+
 module.exports = {
-  handleResponse
+  handleResponse,
+  handleBeautifulProduct
 }
